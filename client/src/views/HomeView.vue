5177b08cb258a030db54706fec33243aa37f074f
Luigigi94 <el.luige94@gmail.com>
2022-04-06 03:59:48 -0500

    First Commit

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <BasicLayout>
+      <h2>Estamos en jombiu</h2>
+    </BasicLayout>
+  </div>
+</template>
+
+<script>
+import BasicLayout from "@/Layouts/BasicLayout";
+
+export default {
+  name: "HomeView",
+  components: {
+    BasicLayout,
+  },
+};
+</script>
